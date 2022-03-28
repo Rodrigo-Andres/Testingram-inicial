d6e97dca963d41291688b36a728c1861daf671fc
Rodrigo Gonzalez <rodrix.andres@gmail.com>
2022-03-28 17:54:55 -0500

    Coding Testingram
    Coding Testingram. Name is not a final decision yet.

---
+++ src/comps/Title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>TestingGram by Rodrigo Andres Gonzalez</h1>
+      <h2>Your Pictures in React</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+      <p>Rodrigo's message: I'm working to create something very functional.</p>
+    </div>
+  )
+}
+
+export default Title;
